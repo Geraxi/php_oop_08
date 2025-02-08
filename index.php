c776de8c96825aff4b60503ec1ca228f8ddc66e0
Geraxi <umbertogeraci0@gmail.com>
2025-02-08 17:33:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,4 +22,29 @@ trait Calculator {
     }
 }
 
+class Rettangolo {
+    use Calculator;
+
+    private $base;
+    private $altezza;
+
+    public function __construct($base, $altezza) {
+        $this->base = $base;
+        $this->altezza = $altezza;
+    }
+
+    public function calcolaArea() {
+        return $this->mul($this->base, $this->altezza);
+    }
+
+    public function calcolaPerimetro() {
+        return $this->sum($this->mul(2, $this->base), $this->mul(2, $this->altezza));
+    }
+
+    public function calcolaDiagonale() {
+        return $this->sqr($this->sum($this->mul($this->base, $this->base), $this->mul($this->altezza, $this->altezza)));
+    }
+}
+
 ?>
+
