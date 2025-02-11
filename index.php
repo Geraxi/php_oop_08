c9cdec15646d11bb70411537fd3bd3e4ab7acc4f
Geraxi <umbertogeraci0@gmail.com>
2025-02-11 20:23:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,30 +22,33 @@ trait Calculator {
     }
 }
 
-class Rectangle{
+class Rectangle {
     use Calculator;
     public $base;
     public $height;
-    public function __construct($b,$h)
-    {
-        $this->base=$b;
-        $this->height= $h;
+
+    public function __construct($b, $h) {
+        $this->base = $b;
+        $this->height = $h;
     }
-    public function getSurfaceArea(){
-        return $this-> mul($this->base, this-> height);
+
+    public function getSurfaceArea() {
+        return $this->mul($this->base, $this->height);
     }
-    public function getPerimeter(){
-        $sum= $this-> sum($this->base, $this->height);
-        $result= $this-> mul($sum,2);
+
+    public function getPerimeter() {
+        $sum = $this->sum($this->base, $this->height);
+        $result = $this->mul($sum, 2);
         return $result;
     }
 
-public function getDiagonal(){
-    $baseSquare= $this-> base**2;
-    $heightSquare= $this->height**2;
-    $sum= $this->sum($baseSquare, $heightSquare);
-    $result=$this->sqr($sum);
-    return $result:
-        }
+    public function getDiagonal() {
+        $baseSquare = $this->base ** 2;
+        $heightSquare = $this->height ** 2;
+        $sum = $this->sum($baseSquare, $heightSquare);
+        $result = $this->sqr($sum);
+        return $result;
+    }
 }
-    
+
+
